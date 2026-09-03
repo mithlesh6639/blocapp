c6a2d921335307a710f3f54c2b5b676ad7f32aa2
Mithlesh <mithlesh6639@gmail.com>
2026-09-03 12:50:05 +0530

    Annotate app build UI
    Added a small comment to mark the start of the UI build in MyApp. This improves readability and makes the app entry widget easier to navigate in the codebase.

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+// Start build UI
   @override
   Widget build(BuildContext context) {
     return RepositoryProvider(
